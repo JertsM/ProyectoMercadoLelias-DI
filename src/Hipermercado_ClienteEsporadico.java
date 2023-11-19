cb19c9f52d47c1743a5d609678d5a66063887ee5
Mauricio Míguez Núñez <124924063+JertsM@users.noreply.github.com>
2023-11-19 19:47:31 +0100

    Actualizar Hipermercado_ClienteEsporadico.java

--- src/Hipermercado_ClienteEsporadico.java
+++ src/Hipermercado_ClienteEsporadico.java
@@ -2,15 +2,51 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Hipermercado_ClienteEsporadico extends JDialog {
-    public Hipermercado_ClienteEsporadico() {
 
+    // Contenedor
+    Container c;
 
+    // Layouts
+    GridBagLayout gbl = new GridBagLayout();
+    GridBagConstraints gbc = new GridBagConstraints();
 
-        Color fondoEmpresaLelia = new Color(113, 212, 93);
+    // Paneles
+    JPanel panelNorte;
+    JPanel panelSur;
+    JPanel panelCentral;
+    JPanel firtsImg;
+    JPanel firstDesc;
+    JPanel secondImg;
+    JPanel secondDesc;
+    JPanel thirdImg;
+    JPanel thirdDesc;
+    JPanel fourthImg;
+    JPanel fourthDesc;
+
+    // Panel superior
+    JLabel cabecera;
+
+    // Panel Central
+    JLabel salmonDesc;
+    JLabel vinoDesc;
+    JLabel arrozDesc;
+    JLabel fileteDesc;
+
+    // Panel inferior
+    JLabel mensajeCancelar;
+    JButton cancelar;
+
+Color fondoEmpresaLelia = new Color(113, 212, 93);
         Color fondoPagina = new Color(174, 250, 132);
         Font fuenteCabecera = new Font("Maiandra GD", Font.BOLD, 40);
         Font fuenteDescripciones = new Font("Maiandra GD", Font.BOLD, 30);
     }
+
+    public Hipermercado_ClienteEsporadico() {
+
+
+
+        
 
     public static void main(String[] args) {
         Hipermercado_ClienteEsporadico lc_hm = new Hipermercado_ClienteEsporadico();
